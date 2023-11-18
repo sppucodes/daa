88e4f178f8d454da4bf793a336db46b0170a5505
Vishal Nanaware <90702847+nanawarevishal@users.noreply.github.com>
2023-11-18 09:39:14 +0530

    added new without re code

--- fib.java
+++ fib.java
@@ -40,3 +40,36 @@ public class FibonacciNonRecursive {
         fibonacci(n);
     }
 }
+
+without re:
+static void fibonaciiSeries(int N){
+
+        int n1 = 0;
+        int n2 = 1;
+
+        // System.out.println(n1);
+        // System.out.println(n2);
+
+        int itr1 = 0;
+
+        for(int i=1;i<=N;i++){
+            System.out.println(n1);
+            int c1 = n1 +n2;
+            n1 = n2;
+            n2 = c1;
+
+            itr1++;
+        }
+
+        // System.out.println("Iterations: "+itr1);
+    }
+
+    public static void main(String[] args) {
+        
+         fibonaciiSeries(10);
+
+        // fibonaciiSeriesRecusrisve(10);
+
+       // fibonaciiSeriesRecusrisve(10);
+    }
+}
